1bd2c31e6a83b9b3f56be2818db4a4d9f4f34bb7
lwl <learnwithlakshman@gmail.com>
2022-03-05 07:15:50 +0530

    Refactored palindrome example

--- src/com/lwl/cj/day3/StringOperations.java
+++ src/com/lwl/cj/day3/StringOperations.java
@@ -2,21 +2,18 @@ package com.lwl.cj.day3;
 
 public class StringOperations {
 
-		public static void main(String[] args) {
-			
-				String data = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-			
-			for(;;) {
-				String str = "";
-				for(int i=1;i<=5;i++) {
-					int num = (int)(Math.random() * data.length());
-					str += data.charAt(num);
-				}
-				
-				boolean isPalindrome = new StringBuffer(str).reverse().toString().equals(str);
-				System.out.println(str +" is Palindrome :"+(isPalindrome? "Yes":"No"));
-				if(isPalindrome)
-					break;
-			}
-	    }
+	public static void main(String[] args) {
+
+		String data = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+		String str = "";
+		for (int i = 1; i <= 3; i++) {
+			int num = (int) (Math.random() * data.length());
+			str += data.charAt(num);
+		}
+
+		boolean isPalindrome = new StringBuffer(str).reverse().toString().equals(str);
+		System.out.println(str + " is Palindrome :" + (isPalindrome ? "Yes" : "No"));
+
+	}
 }
