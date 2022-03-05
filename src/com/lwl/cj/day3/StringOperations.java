db4755c8461a6d968581576803df11f013a4a346
lwl <learnwithlakshman@gmail.com>
2022-03-05 12:03:54 +0530

    Refactored String example

--- src/com/lwl/cj/day3/StringOperations.java
+++ src/com/lwl/cj/day3/StringOperations.java
@@ -3,17 +3,13 @@ package com.lwl.cj.day3;
 public class StringOperations {
 
 	public static void main(String[] args) {
-
 		String data = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
 		String str = "";
 		for (int i = 1; i <= 3; i++) {
 			int num = (int) (Math.random() * data.length());
 			str += data.charAt(num);
 		}
-
-		boolean isPalindrome = new StringBuffer(str).reverse().toString().equals(str);
+		boolean isPalindrome = new StringBuilder(str).reverse().toString().equals(str);
 		System.out.println(str + " is Palindrome :" + (isPalindrome ? "Yes" : "No"));
-
 	}
 }
